88d471e0b80623876c9065f036f9ee33bd36d3b4
Ben Schuman <ben@jeffschuman.com>
2022-12-23 01:14:19 -0500

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,60 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import { useState } from "react";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+
+const Navbar = () => {
+  const { data: session } = useSession();
+  const [hidePopout, setHidePopout] = useState(true);
+
+  return (
+    <>
+      <div className="flex bg-white h-12 shadow-md items-center">
+        {session?.user?.image && (
+          <>
+            <div
+              className={`ml-auto mr-5 rounded-full select-none cursor-pointer ${
+                !hidePopout && "ring-4 ring-gray-300"
+              }`}
+              onClick={() => setHidePopout(!hidePopout)}
+            >
+              <Image
+                width={35}
+                height={35}
+                className="rounded-full"
+                src={session.user.image}
+              />
+            </div>
+            {/* Popout user profile */}
+            <div
+              className={`flex flex-col absolute p-2 top-14 right-5 bg-white shadow-lg rounded-md w-56 border border-gray-200 ${
+                hidePopout && "hidden"
+              }`}
+            >
+              <div className="flex flex-col p-2 border-b-2 border-gray-600">
+                <p className="text-gray-800">{session?.user?.name}</p>
+                <p className="text-sm text-gray-600">{session?.user?.email}</p>
+              </div>
+              <div className="flex ml-auto mr-2 py-1 items-center group">
+                <button className="mr-1" onClick={signOut}>
+                  Sign Out
+                </button>
+                <ArrowRightOnRectangleIcon className="h-5 -translate-x-10 opacity-0 text-gray-800 transition duration-150 group-hover:translate-x-0 group-hover:opacity-100" />
+              </div>
+            </div>
+          </>
+        )}
+        {!session?.user && (
+          <button
+            className="ml-auto mr-5 cursor-pointer hover:text-gray-700 active:text-gray-600"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  adapter: FirestoreAdapter({
+    apiKey: process.env.FIREBASE_API_KEY,
+    appId: process.env.FIREBASE_APP_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  }),
+});
